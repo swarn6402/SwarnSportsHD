f4f694a32b2a4b25b8ddff6ad5c0dd44f372e8a4
swarn6402 <swarnjeettiwary01@gmail.com>
2026-02-10 12:04:53 +0530

    Add the Js file for the logic

---
+++ SwarnSports/frontend/script.js
@@ -0,0 +1,5 @@
+﻿// TODO: Fetch aggregated links from backend endpoint.
+// TODO: Render link list into #links-container.
+// TODO: Add loading and error states.
+
+console.log("SwarnSports frontend scaffold ready.");
